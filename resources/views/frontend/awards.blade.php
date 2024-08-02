7aba98aeadc144c25573b8d7de64cc5ff9288c31
Shailesh Gupta <shaileshhq@gmail.com>
2024-08-02 11:45:59 +0530

    css

--- resources/views/frontend/awards.blade.php
+++ resources/views/frontend/awards.blade.php
@@ -7,10 +7,10 @@
             <img src="{{ asset('public/frontend_css/assets/img/breadcrumb/br-shape-3.webp')}}" alt="Image" class="br-shape-three">
             <div class="container">
                 <div class="breadcrumb-content">
-                    <h2>Blog</h2>
+                    <h2>Awards</h2>
                     <ul class="breadcrumb-menu list-style">
-                        <li><a href="index.php">Home</a></li>
-                        <li>Blog</li>
+                        <li><a href="{{route('index')}}">Home</a></li>
+                        <li>Awards & Certificate</li>
                     </ul>
                 </div>
             </div>
